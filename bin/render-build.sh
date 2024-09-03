7249cdcc50ab8d34f70c0e6a50b03f932baadb69
RajMankar-8 <rajmankar6858@gmail.com>
2024-09-04 01:32:26 +0530

    test-10

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
 set -o errexit
 
+# Install necessary gems
 bundle install 
+
+# Clean up old assets to start with a fresh state
+bundle exec rails assets:clobber
+
+# Precompile the assets
 bundle exec rails assets:precompile
+
+# Optionally clean up old assets after precompilation
 bundle exec rails assets:clean
