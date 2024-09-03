8f9f1786b028f17a9d3cfc3442f2ffce08004ff3
RajMankar-8 <rajmankar6858@gmail.com>
2024-09-04 01:12:19 +0530

    test-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
  has_many :workers, dependent: :destroy
  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
+
+ after_create :confirm_user
+
+  def confirm_user
+    self.confirm
+    save
+  end
 end
